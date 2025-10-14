af62def92792255410b358876911c93456ed1e58
Abuelqasim Mohamed <abolkasm11@gmail.com>
2025-10-14 05:53:26 +0200

    before of 151

--- admin/orders/approve.php
+++ admin/orders/approve.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include "../../connect.php";
+//Beofre of 151
 //What is the prupose of this file?
 //The purpose of this file is that when the user made an order this order will be waiting for approvel 
 //when the admin click on the approve button the order status will be change from 0 to 1 .
@@ -15,7 +16,9 @@ $data=[
 ];
 updateData("orders",$data,"order_id='$order_id' And order_status=0");
 //after of that we have update the status of the order we have to send notfication to the user to tell him what is going on .
-$user_id= filterRequest('user_id');sendFcmNotification(
+$user_id= filterRequest('user_id');
+//Know will use this function to send the notfication.
+sendFcmNotification(
   "Order Update",
   "Your order #$order_id has been accepted and is now being prepared.",
   "users$user_id",
