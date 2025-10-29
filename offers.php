06a27c4efe387fc57039cbf24223d7dced1748c3
Abuelqasim Mohamed <abolkasm11@gmail.com>
2025-10-29 06:30:38 +0200

    before of 167

--- offers.php
+++ offers.php
@@ -1,26 +1,38 @@
 <?php
-// Error reporting (Good practice for development)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-//Before of 165
-include "connect.php";
-//The goal of this file is to be able to get all of the products that there is an offer on them
+//Before of 115
+// تضمين ملف الاتصال بقاعدة البيانات
+include "./connect.php";
+//Before of 85
 
-
-//Notice that here we bring the data of all of the products that they don't belong to any spcfic user.we just want to display
-//All of the products that there is a discount on them.
-
-
-//Why did we bring the data fromt he products table instead of bring it from the product_view?
-//Because of here in the offers page we just want to display all of the products that there is a discount 
-//over them.we don't cart about that this product is belonging to that catogery.
-//So bring the data directly from the products table benfits are:
-//Make the perfomance faster because there is no join operation here .
-//Make the code simpler and easy to understand.
-getAllData(
-    "products",
-    "product_discount!=0"
+$stmt = $con->prepare(
+    " SELECT products1_view.*, 1 AS fav ,
+    (product_price-(product_price*product_discount/100)) As priceAfterDiscount
+     FROM products1_view 
+     INNER JOIN favorite 
+     ON favorite.product_id = products1_view.products_id 
+     And product_discount!=0
+     UNION
+     SELECT products1_view.*, 0 AS fav ,
+     (product_price-(product_price*product_discount/100)) As priceAfterDiscount
+     FROM products1_view
+     WHERE
+       product_discount!=0 And
+      products_id NOT IN (
+         SELECT products1_view.products_id 
+         FROM products1_view 
+         INNER JOIN favorite 
+         ON favorite.product_id = products1_view.products_id  )"
 );
 
+$stmt->execute();
+$products=$stmt->fetchAll(PDO::FETCH_ASSOC);
+$count=$stmt->rowCount();
+if($count>0){
+  echo json_encode(array("status" => "success", "data" => $products));
+}else{
+echo json_encode(array("status" => "failure"));
+}
 ?>
